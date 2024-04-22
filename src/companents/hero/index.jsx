a8e4df1cb2ecad54868e23f9d7bd83aa2695f0fd
Admeral2000 <husniddinzoirjonov@gmail.com>
2024-04-22 14:03:45 +0500

    created Modalniy screen

--- src/companents/hero/index.jsx
+++ src/companents/hero/index.jsx
@@ -1,32 +1,72 @@
 import { useState } from "react";
+import cls from "@/companents/hero/hero.module.css";
+
+// JSX elemnt qilib yaratildi
+// const Modal=({open, setOpen})=>(
+//   <div className={`${cls.overlay} ${cls.animated} ${open?`${cls.show}`:``}`}>
+//         <div className={cls.modal}>
+//           <svg onClick={()=>setOpen(false)} height={200} width={200} viewBox="0 0 200 200" >
+//             <title />
+//             <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+//           </svg>
+//         <picture> <img width={400} src="https://picsum.photos/200/120"  /></picture> 
+//         </div>
+//       </div>
+// )
+
+// Bunda Jsx elemtida rasmni dinamik chaqirildi
+const Modal=({open, setOpen, children})=>(
+  <div className={`${cls.overlay} ${cls.animated} ${open?`${cls.show}`:``}`}>
+        <div className={cls.modal}>
+          <svg onClick={()=>setOpen(false)} height={200} width={200} viewBox="0 0 200 200" >
+            <title />
+            <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+          </svg>
+       {children} 
+        </div>
+      </div>
+)
 
 export default function Hero() {
-    // let count=10;
-    const [count, setCount]=useState(0)
-    const onClickPlus=()=>{
-    // count++;
-    // console.log(count);
-    setCount(count+1)
-    }
-    const onClickMinus=()=>{
-        if (count > 0) {
-            setCount(count - 1);
-        }  
-    }
+    let [open,setOpen]=useState(false)
     
     return(
-        <div className='container bg-slate-500'>
-            <div className="bg-yellow-50 mx-auto py-[80px] px-[90px] text-center  max-w-prose ... ">
-            <h1 className="mx-auto text-center text-[44px]">Ишотчик:</h1>
-                <h1 className="mx-auto text-[84px] mt-[20px]">{count}</h1>
-               <div className="flex items-center justify-between gap-[54px] mt-[30px]">
-                <button onClick={onClickMinus} className="rounded-[12px] bg-red-500 py-[15px] px-[57px] shadow-lg shadow-blue-500/50 shadow-winner text-[30px]"  type="button">-</button>
-                <button onClick={onClickPlus} className="rounded-[12px] bg-green-600 py-[15px] px-[57px] shadow-lg shadow-red-500/50 shadow-xl text-[30px]"  type="button">+</button>
-               </div> 
-            </div>
-        </div>
-       
-      
+        <div className={cls.App}>
+        <button onClick={()=>setOpen(true)} className={cls.openModalBtn}  type="button">✨ открытки окно</button>
+        {/* Yangidan reneder qiladi, agar Modal JSX elementi orqali yaratilsa */}
+        {/* {open && <Modal open={open} setOpen={setOpen} />} */}
         
-    )
+        {/* Agar open && JSX elementi oldidan yozilsa animatsiya ishlamaydi, amal juda tez bajariladi */}
+        {/* {open &&( <div className={cls.overlay}>
+        <div className={cls.modal}>
+          <svg onClick={()=>setOpen(false)} height={200} width={200} viewBox="0 0 200 200" >
+            <title />
+            <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+          </svg>
+        <picture> <img width={400} src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif"  /></picture> 
+        </div>
+      </div>
+      )} */}
+
+      {/* Bu yerda open && olib tashlanagn,  va show className={show} qo'shildi, shuning uchun u render bo'lmaydi va animatsiya ishlaydi */}
+     {/* <div className={`${cls.overlay} ${cls.animated} ${open?`${cls.show}`:``}`}>
+        <div className={cls.modal}>
+          <svg onClick={()=>setOpen(false)} height={200} width={200} viewBox="0 0 200 200" >
+            <title />
+            <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+          </svg>
+        <picture> <img width={400} src="https://picsum.photos/200/120"  /></picture> 
+        </div>
+      </div> */}
+     
+
+     {/* Jsx elementida element dinamik qilindi, children o'rniga element qo'ydik */}
+     <Modal open={open} setOpen={setOpen}> 
+     <picture> <img width={400} src="https://picsum.photos/200/120"  /></picture>
+     <h3>This picture</h3> 
+     </Modal>
+     
+      </div>
+    
+);
 }
